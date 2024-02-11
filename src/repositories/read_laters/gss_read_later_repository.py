05b2730f54d7a0b754e98f1518f633724216f2f7
hellomyzn <em711th@icloud.com>
2024-02-12 02:29:14 +0900

    update: sheet name and columns

--- src/repositories/read_laters/gss_read_later_repository.py
+++ src/repositories/read_laters/gss_read_later_repository.py
@@ -18,13 +18,15 @@ from repositories import GssBaseRepository
 class GssReadLaterRepository(GssBaseRepository):
     """gss read later repository"""
 
-    SHEET_NAME = "test"
+    SHEET_NAME = "read laters"
 
     KEY_ID = "id"
     KEY_TITLE = "title"
     KEY_URL = "url"
     KEY_CREATE_AT = "create_at"
-    COLUMNS = [KEY_ID, KEY_TITLE, KEY_URL, KEY_CREATE_AT]
+    KEY_READ = "read"
+    KEY_READ_AT = "read_at"
+    COLUMNS = [KEY_ID, KEY_TITLE, KEY_URL, KEY_CREATE_AT, KEY_READ, KEY_READ_AT]
 
     def __init__(self):
         super().__init__(self.SHEET_NAME, self.COLUMNS)
