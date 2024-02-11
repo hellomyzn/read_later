d9e9120d10b5cdc3925504d874335b1e8dfbdbb4
hellomyzn <em711th@icloud.com>
2024-02-11 19:46:17 +0900

    add: gss read later repo

---
+++ src/repositories/read_laters/gss_read_later_repository.py
@@ -0,0 +1,22 @@
+"""repositories.read_laters.gss_read_later_repository"""
+#########################################################
+# Builtin packages
+#########################################################
+# (None)
+
+#########################################################
+# 3rd party packages
+#########################################################
+# (None)
+
+#########################################################
+# Own packages
+#########################################################
+from repositories import GssBaseRepository
+
+
+class GssReadLaterRepository(GssBaseRepository):
+    """gss read later repository"""
+
+    def __init__(self):
+        pass
