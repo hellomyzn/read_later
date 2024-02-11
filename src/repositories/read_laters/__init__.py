03cae1ff1035e128788c3cfcf7deafc63eab896d
hellomyzn <em711th@icloud.com>
2024-02-11 19:35:14 +0900

    add: csv read later repo

---
+++ src/repositories/read_laters/__init__.py
@@ -0,0 +1,4 @@
+"""repositories"""
+
+from .csv_read_later_repository import CsvReadLaterRepository
+from .gss_read_later_repository import GssReadLaterRepository
---
+++ src/repositories/read_laters/csv_read_later_repository.py
@@ -0,0 +1,29 @@
+"""repositories.read_laters.csv_read_later_repository"""
+#########################################################
+# Builtin packages
+#########################################################
+# (None)
+
+#########################################################
+# 3rd party packages
+#########################################################
+# (None)
+
+#########################################################
+# Own packages
+#########################################################
+from repositories import CsvBaseRepository
+
+
+class CsvReadLaterRepository(CsvBaseRepository):
+    """csv read later repository"""
+    CSV_FILE_PATH = "/opt/work/src/csv/read_later.csv"
+
+    KEY_ID = "id"
+    KEY_TITLE = "title"
+    KEY_URL = "url"
+    KEY_CREATE_AT = "create_at"
+    HEADER = [KEY_ID, KEY_TITLE, KEY_URL, KEY_CREATE_AT]
+
+    def __init__(self):
+        super().__init__(self.CSV_FILE_PATH, self.HEADER)
