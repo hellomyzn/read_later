a38142a19c7f515b716f0d03cb08a2f51891f4b8
hellomyzn <em711th@icloud.com>
2024-02-12 02:18:12 +0900

    update: init

--- src/repositories/read_laters/gss_read_later_repository.py
+++ src/repositories/read_laters/gss_read_later_repository.py
@@ -18,5 +18,13 @@ from repositories import GssBaseRepository
 class GssReadLaterRepository(GssBaseRepository):
     """gss read later repository"""
 
+    SHEET_NAME = "test"
+
+    KEY_ID = "id"
+    KEY_TITLE = "title"
+    KEY_URL = "url"
+    KEY_CREATE_AT = "create_at"
+    COLUMNS = [KEY_ID, KEY_TITLE, KEY_URL, KEY_CREATE_AT]
+
     def __init__(self):
-        pass
+        super().__init__(self.SHEET_NAME, self.COLUMNS)
